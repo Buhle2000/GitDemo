a27954d244c973ebec0258d4ec4dd475985fecd8
Buhle2000 <buhle.mxube@cestasoft.com>
2024-12-11 10:33:06 +0200

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      String name1="Buhle";
+      String name2="Mxube";
+      String result="";
+      
+      result = name1 + " " + name2;
+      
+      System.out.println(result);
+  }
+}
